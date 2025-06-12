c6e936b0a07c2e13dfefceae0eeb5ddfdad17b45
aziyanck <aziyan916@gmail.com>
2025-06-12 21:13:05 +0530

    bug fix

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -5,10 +5,10 @@ function Feature() {
         <div className="feature  w-[100vw] h-[auto] flex flex-col justify-center items-center overflow-hidden ">
 
             <div className="flex p-6 w-[90vw] justify-center items-center">
-                <div className="flex flex-col justify-around  items-center z-[1] bg-white/10 backdrop-blur-lg rounded-xl shadow-xl shadow-blue-300 group-[.dark-mode]:shadow-none group-[.dark-mode]:shadow-2xl max-w-[80%] h-[auto]">
-                    <div className="ft py-4 text-cyan-900 group-[.dark-mode]:text-white text-center ">
+                <div className="flex flex-col justify-around  items-center z-[1] bg-white/10 backdrop-blur-lg rounded-xl shadow-xl shadow-blue-300 group-[.dark-mode]:shadow-none group-[.dark-mode]:shadow-2xl max-w-[80%] h-[auto] p-4">
+                    <div className="ft pb-4 text-cyan-900 group-[.dark-mode]:text-white text-center ">
                         <h1 data-aos="fade-up" className=" text-center text-lg md:text-2xl poppins-extrabold p-4">We make your home smarter and your life easier!</h1>
-                        <p data-aos="fade-up" data-aos-delay="100" className="mx-6 text-sm md:text-base leading-loose poppins-mededium md:mx-20">
+                        <p data-aos="fade-up" data-aos-delay="100" className="mx-6 text-justify text-xs md:text-base leading-loose poppins-mededium md:mx-20">
                             Smart Home technology is not reserved for tech savvy consumers or brand new homes. Smart home devices are surprisingly affordable and can add a touch of modern convenience to any existing home. Industech Automations can streamline the sale, install, set-up and integration of these devices so you have a one stop solution for your all home automation requirements.
                             Contact us today and let Industech Automations upgrade your existing home's functionality with today's latest conveniences, all with minimal invasion and complete satisfaction.
                         </p>
@@ -16,7 +16,7 @@ function Feature() {
 
                     <div className="ft py-4 text-cyan-900 group-[.dark-mode]:text-white text-center">
                         <h1 data-aos="fade-up" className=" text-center text-lg md:text-2xl poppins-extrabold p-4">What We Do?</h1>
-                        <p data-aos="fade-up" data-aos-delay="100" className="mx-6 text-sm md:text-base leading-loose poppins-mededium md:mx-20">
+                        <p data-aos="fade-up" data-aos-delay="100" className="mx-6 text-justify text-xs md:text-base leading-loose poppins-mededium md:mx-20">
                             Using your home's existing wiring, Industech Autoations can upgrade switches, sensors, dimmers, speakers and more to turn any home into a smart home with packages and options to suit the budget conscious consumer. We carry the latest digital device assistants - like Amazon Echo and Google Assistant – enhancing their effectiveness to create true automated conveniences. Industech Automations will transform the functionality of your home with customized solutions tailored to your needs, your budget and your satisfaction.
 
                         </p>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -37,12 +37,12 @@ function Home() {
             <div className="up w-[90vw] min-h-[55vh] md:min-h-[60vh] md:bg-amber-300 bg-red-500 ">
 
             </div>
-            <div className="down text-center flex flex-col group-[.dark-mode]:text-white text-cyan-900  items-center w-[90vw] h-[auto] max-w-[95vw] p-4 [&>*]:mb-3 gradient">
+            <div className="down text-center flex flex-col group-[.dark-mode]:text-white text-cyan-900  items-center w-[90vw] h-[50vh] max-w-[95vw] p-4 [&>*]:mb-3 gradient">
                 <h1 data-aos="fade-up" className="title text-lg md:text-3xl pb-8 md:pb-2 poppins-extrabold leading-relaxed">
                     When your home thinks with you,<br /> life just flows easier.
                 </h1>
                 <h2 data-aos="fade-right" className='text-base md:text-lg poppins-bold pb-5 md:pb-2'>“Let <span className="bg-gradient-to-r from-indigo-500 to-blue-500 bg-clip-text text-transparent  group-[.dark-mode]:bg-gradient-to-r group-[.dark-mode]:from-fuchsia-500 group-[.dark-mode]:to-pink-500 poppins-bold">IndusTech Automations</span>  turn your everyday moments into extraordinary ones.”</h2>
-                <h4 data-aos="fade-up">
+                <h4 >
                     <span ref={typedRef} className=" text-amber-600 inline text-lg poppins-bold"></span>
 
                 </h4>
